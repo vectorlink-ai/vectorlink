2a92a2d515f0cac431e25768021a7b304b87ed40
Maren van Otterdijk <maren.van.otterdijk@gmail.com>
2024-09-19 13:47:15 +0200

    benchmark various simd widths

--- benches/compare.rs
+++ benches/compare.rs
@@ -6,7 +6,7 @@ use rand::{distributions::Uniform, rngs::StdRng, Rng, SeedableRng};
 use test::Bencher;
 
 #[bench]
-fn bench_dot_product(b: &mut Bencher) {
+fn bench_dot_product_8(b: &mut Bencher) {
     const VECS: usize = 10000;
     let mut rng = StdRng::seed_from_u64(2024);
     let range = Uniform::from(-1.0..1.0);
@@ -16,7 +16,55 @@ fn bench_dot_product(b: &mut Bencher) {
     b.iter(|| {
         for i in 0..VECS {
             let v2 = &vs2[(i * 1024)..((i + 1) * 1024)];
-            vecmath::dot_product_1024(&v1, v2);
+            vecmath::dot_product_1024_8(&v1, v2);
+        }
+    });
+}
+
+#[bench]
+fn bench_dot_product_16(b: &mut Bencher) {
+    const VECS: usize = 10000;
+    let mut rng = StdRng::seed_from_u64(2024);
+    let range = Uniform::from(-1.0..1.0);
+    let v1: Vec<f32> = (0..1024).map(|_| rng.sample(range)).collect();
+    let vs2: Vec<f32> = (0..1024 * VECS).map(|_| rng.sample(range)).collect();
+
+    b.iter(|| {
+        for i in 0..VECS {
+            let v2 = &vs2[(i * 1024)..((i + 1) * 1024)];
+            vecmath::dot_product_1024_16(&v1, v2);
+        }
+    });
+}
+
+#[bench]
+fn bench_dot_product_32(b: &mut Bencher) {
+    const VECS: usize = 10000;
+    let mut rng = StdRng::seed_from_u64(2024);
+    let range = Uniform::from(-1.0..1.0);
+    let v1: Vec<f32> = (0..1024).map(|_| rng.sample(range)).collect();
+    let vs2: Vec<f32> = (0..1024 * VECS).map(|_| rng.sample(range)).collect();
+
+    b.iter(|| {
+        for i in 0..VECS {
+            let v2 = &vs2[(i * 1024)..((i + 1) * 1024)];
+            vecmath::dot_product_1024_32(&v1, v2);
+        }
+    });
+}
+
+#[bench]
+fn bench_dot_product_64(b: &mut Bencher) {
+    const VECS: usize = 10000;
+    let mut rng = StdRng::seed_from_u64(2024);
+    let range = Uniform::from(-1.0..1.0);
+    let v1: Vec<f32> = (0..1024).map(|_| rng.sample(range)).collect();
+    let vs2: Vec<f32> = (0..1024 * VECS).map(|_| rng.sample(range)).collect();
+
+    b.iter(|| {
+        for i in 0..VECS {
+            let v2 = &vs2[(i * 1024)..((i + 1) * 1024)];
+            vecmath::dot_product_1024_64(&v1, v2);
         }
     });
 }
@@ -30,8 +78,8 @@ impl Indirection {
     #[inline(always)]
     fn dot_product(&self, v1: &[f32], v2: &[f32]) -> f32 {
         match self {
-            Indirection::Size1024 => vecmath::dot_product_1024(v1, v2),
-            Indirection::Size1536 => vecmath::dot_product_1536(v1, v2),
+            Indirection::Size1024 => vecmath::dot_product_1024_16(v1, v2),
+            Indirection::Size1536 => vecmath::dot_product_1536_16(v1, v2),
         }
     }
 }
@@ -62,7 +110,7 @@ struct DoctProduct1024;
 impl DotProduct for DoctProduct1024 {
     #[inline(always)]
     fn dot_product(&self, v1: &[f32], v2: &[f32]) -> f32 {
-        vecmath::dot_product_1024(v1, v2)
+        vecmath::dot_product_1024_16(v1, v2)
     }
 }
 
--- src/vecmath.rs
+++ src/vecmath.rs
@@ -1,8 +1,13 @@
-use std::simd::{f32x16, num::SimdFloat};
+use std::simd::{f32x16, f32x32, f32x64, f32x8, num::SimdFloat};
 
 use unroll::unroll_for_loops;
 
-macro_rules! dot_product_n {
+// both unroll and (especially) inlining drastically improve speed
+// when calling comparisons in a loop.
+// tests indicate,
+// - unroll: 10% speedup
+// - inline: 1000% speedup
+macro_rules! dot_product_n_16 {
     ($name:ident, $n:literal) => {
         #[unroll_for_loops]
         #[inline(always)]
@@ -17,6 +22,57 @@ macro_rules! dot_product_n {
         }
     };
 }
+macro_rules! dot_product_n_8 {
+    ($name:ident, $n:literal) => {
+        #[unroll_for_loops]
+        #[inline(always)]
+        pub fn $name(left: &[f32], right: &[f32]) -> f32 {
+            let mut sum = <f32x8>::splat(0.);
+            for x in 0..$n {
+                let l = <f32x8>::from_slice(&left[x * 8..(x + 1) * 8]);
+                let r = <f32x8>::from_slice(&right[x * 8..(x + 1) * 8]);
+                sum += l * r;
+            }
+            sum.reduce_sum()
+        }
+    };
+}
+macro_rules! dot_product_n_32 {
+    ($name:ident, $n:literal) => {
+        #[unroll_for_loops]
+        #[inline(always)]
+        pub fn $name(left: &[f32], right: &[f32]) -> f32 {
+            let mut sum = <f32x32>::splat(0.);
+            for x in 0..$n {
+                let l = <f32x32>::from_slice(&left[x * 32..(x + 1) * 32]);
+                let r = <f32x32>::from_slice(&right[x * 32..(x + 1) * 32]);
+                sum += l * r;
+            }
+            sum.reduce_sum()
+        }
+    };
+}
+macro_rules! dot_product_n_64 {
+    ($name:ident, $n:literal) => {
+        #[unroll_for_loops]
+        #[inline(always)]
+        pub fn $name(left: &[f32], right: &[f32]) -> f32 {
+            let mut sum = <f32x64>::splat(0.);
+            for x in 0..$n {
+                let l = <f32x64>::from_slice(&left[x * 64..(x + 1) * 64]);
+                let r = <f32x64>::from_slice(&right[x * 64..(x + 1) * 64]);
+                sum += l * r;
+            }
+            sum.reduce_sum()
+        }
+    };
+}
 
-dot_product_n!(dot_product_1536, 96);
-dot_product_n!(dot_product_1024, 64);
+dot_product_n_8!(dot_product_1536_8, 192);
+dot_product_n_8!(dot_product_1024_8, 128);
+dot_product_n_16!(dot_product_1536_16, 96);
+dot_product_n_16!(dot_product_1024_16, 64);
+dot_product_n_32!(dot_product_1536_32, 48);
+dot_product_n_32!(dot_product_1024_32, 32);
+dot_product_n_64!(dot_product_1536_64, 24);
+dot_product_n_64!(dot_product_1024_64, 16);

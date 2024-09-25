e40bbf6225c3ce19d40e71c6c1f956e002aba2b1
Maren van Otterdijk <maren.van.otterdijk@gmail.com>
2024-09-25 16:29:47 +0200

    centroid search benchmark

--- benches/centroid.rs
+++ benches/centroid.rs
@@ -5,6 +5,7 @@ use hnsw_redux::{
     hnsw::{BuildParams, Hnsw},
     layer::SearchParams,
     test_util::random_vectors,
+    vectors::Vector,
 };
 use test::Bencher;
 
@@ -41,4 +42,16 @@ fn bench_improve(b: &mut Bencher) {
     let mut hnsw = Hnsw::generate(&bp, &comparator);
     let sp = SearchParams::default();
     b.iter(|| hnsw.improve_neighbors_in_all_layers(&sp, &comparator));
+}
+
+#[bench]
+fn bench_centroid_search(b: &mut Bencher) {
+    let number_of_vecs = u16::MAX as usize;
+    let vecs = random_vectors(number_of_vecs, 8, 0x533D);
+    let comparator = EuclideanDistance8x8::new(&vecs);
+    let bp = BuildParams::default();
+    let mut hnsw = Hnsw::generate(&bp, &comparator);
+    let sp = SearchParams::default();
+    let vec = &random_vectors(1, 8, 0x12345)[0];
+    b.iter(|| hnsw.search_from_initial(Vector::Slice(vec), &sp, &comparator));
 }

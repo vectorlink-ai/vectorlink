04f2145d644e9521c1abab267fb16d342884ab2e
Maren van Otterdijk <maren.van.otterdijk@gmail.com>
2024-09-25 19:58:26 +0200

    missing benchmark files

---
+++ benches/bench_pq.rs
@@ -0,0 +1,53 @@
+#![feature(test)]
+
+extern crate test;
+use hnsw_redux::{
+    comparator::{
+        EuclideanDistance8x8, NewDotProductCentroidDistanceCalculator8, NewEuclideanDistance8x8,
+        NewMemoizedComparator128,
+    },
+    hnsw::BuildParams,
+    layer::SearchParams,
+    pq::{create_pq, VectorRangeIndexableForVectors},
+    test_util::random_vectors,
+    vectors::Vector,
+};
+use test::Bencher;
+
+#[bench]
+fn bench_pq(b: &mut Bencher) {
+    let number_of_vecs = 100_000;
+    let vecs = random_vectors(number_of_vecs, 1024, 0x533D);
+    let vector_indexable = VectorRangeIndexableForVectors(&vecs);
+    let vector_stream = vecs.iter();
+    let centroid_count = u16::MAX as usize;
+    let centroid_byte_size = 8 * std::mem::size_of::<f32>();
+    let centroid_build_params = BuildParams::default();
+    let quantized_build_params = BuildParams::default();
+    let quantizer_search_params = SearchParams::default();
+
+    let pq = create_pq::<
+        NewEuclideanDistance8x8,
+        NewMemoizedComparator128,
+        NewDotProductCentroidDistanceCalculator8,
+        _,
+        _,
+    >(
+        &vector_indexable,
+        vector_stream,
+        centroid_count,
+        centroid_byte_size,
+        &centroid_build_params,
+        &quantized_build_params,
+        quantizer_search_params,
+        0x533D,
+    );
+
+    let vec = &random_vectors(1, 1024, 0x12345)[0];
+    let mut quantized = vec![0_u8; 256];
+    let comparator = EuclideanDistance8x8::new(pq.centroids());
+    pq.quantizer().quantize(vec, &comparator, &mut quantized);
+
+    let sp = SearchParams::default();
+    b.iter(|| pq.search_from_initial_quantized(Vector::Slice(&quantized), &sp, &comparator));
+}
---
+++ benches/quantize.rs
@@ -0,0 +1,41 @@
+#![feature(test)]
+
+extern crate test;
+use hnsw_redux::{
+    comparator::{NewEuclideanDistance8x8, VectorComparatorConstructor},
+    hnsw::{BuildParams, Hnsw},
+    layer::SearchParams,
+    pq::{centroid_finder, Quantizer, VectorRangeIndexableForVectors},
+    test_util::random_vectors,
+};
+use test::Bencher;
+
+#[bench]
+fn bench_quantization(b: &mut Bencher) {
+    let number_of_vecs = 10000;
+    let vectors = random_vectors(number_of_vecs, 1024, 0x533D);
+    let vector_indexable = VectorRangeIndexableForVectors(&vectors);
+    let centroid_count = u16::MAX as usize;
+    let centroid_byte_size = 8 * std::mem::size_of::<f32>();
+    let centroid_build_params = BuildParams::default();
+    let quantizer_search_params = SearchParams {
+        parallel_visit_count: 8,
+        visit_queue_len: 30,
+        search_queue_len: 10,
+        circulant_parameter_count: 8,
+    };
+    let centroids = centroid_finder(
+        &vector_indexable,
+        centroid_count,
+        centroid_byte_size,
+        0xD335,
+    );
+
+    let centroid_comparator = NewEuclideanDistance8x8::new_from_vecs(&centroids);
+    let centroid_hnsw = Hnsw::generate(&centroid_build_params, &centroid_comparator);
+    let quantizer = Quantizer::new(centroid_hnsw, quantizer_search_params);
+    b.iter(move || {
+        let vector_stream = vectors.iter();
+        quantizer.quantize_all(vectors.num_vecs(), vector_stream, &centroid_comparator)
+    });
+}

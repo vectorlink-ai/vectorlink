aacce7b3845438fb749ee00496fb356c62f90f3e
Gavin Mendel-Gleason <gavin@terminusdb.com>
2024-09-27 09:24:04 +0200

    Add construction bench

--- hnsw-redux/benches/bench_pq.rs
+++ hnsw-redux/benches/bench_pq.rs
@@ -14,7 +14,38 @@ use hnsw_redux::{
 use test::Bencher;
 
 #[bench]
-fn bench_pq(b: &mut Bencher) {
+fn bench_pq_construction(b: &mut Bencher) {
+    let number_of_vecs = 100_000;
+    let vecs = random_vectors(number_of_vecs, 1024, 0x533D);
+    let vector_indexable = VectorRangeIndexableForVectors(&vecs);
+    let centroid_count = u16::MAX as usize;
+    let centroid_byte_size = 8 * std::mem::size_of::<f32>();
+    let centroid_build_params = BuildParams::default();
+    let quantized_build_params = BuildParams::default();
+    let quantizer_search_params = SearchParams::default();
+    b.iter(|| {
+        let vector_stream = vecs.iter();
+        let _pq = create_pq::<
+            NewEuclideanDistance8x8,
+            NewMemoizedComparator128,
+            NewDotProductCentroidDistanceCalculator8,
+            _,
+            _,
+        >(
+            &vector_indexable,
+            vector_stream,
+            centroid_count,
+            centroid_byte_size,
+            &centroid_build_params,
+            &quantized_build_params,
+            quantizer_search_params,
+            0x533D,
+        );
+    });
+}
+
+#[bench]
+fn bench_pq_search(b: &mut Bencher) {
     let number_of_vecs = 100_000;
     let vecs = random_vectors(number_of_vecs, 1024, 0x533D);
     let vector_indexable = VectorRangeIndexableForVectors(&vecs);

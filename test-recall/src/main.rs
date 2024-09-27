4df40f33fd248e59d800a7722e3f9eea6f05fc35
Maren van Otterdijk <maren.van.otterdijk@gmail.com>
2024-09-27 10:55:34 +0200

    actually check in new files this time

---
+++ test-recall/src/main.rs
@@ -0,0 +1,31 @@
+use std::io;
+
+use clap::Parser;
+use hnsw_redux::{
+    index::{Index, IndexConfiguration},
+    params::SearchParams,
+};
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Command {
+    #[arg(long)]
+    vector_directory: String,
+    #[arg(long)]
+    hnsw_root_directory: String,
+    #[arg(long)]
+    name: String,
+}
+fn main() -> io::Result<()> {
+    let args = Command::parse();
+    let index = IndexConfiguration::load(
+        &args.name,
+        &args.hnsw_root_directory,
+        &args.vector_directory,
+    )?;
+    let sp = SearchParams::default();
+    let recall = index.test_recall(&sp, 0x533D);
+    println!("{recall}");
+
+    Ok(())
+}

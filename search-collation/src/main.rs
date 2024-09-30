7e293f7015f82e9e746e331155e7d9c0d34a6bb5
Gavin Mendel-Gleason <gavin@terminusdb.com>
2024-09-30 11:35:54 +0200

    Add search collation

---
+++ search-collation/src/main.rs
@@ -0,0 +1,86 @@
+use byteorder::{BigEndian, NativeEndian, ReadBytesExt};
+use clap::Parser;
+use csv::Writer;
+use std::io::{prelude::*, SeekFrom};
+use std::os::unix::fs::MetadataExt;
+use std::{fs::File, io};
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Command {
+    #[arg(long)]
+    record_file: String,
+    #[arg(long)]
+    record_index_file: String,
+    #[arg(long)]
+    search_file: String,
+    #[arg(long)]
+    output_table: String,
+    #[arg(long)]
+    fields: Option<String>,
+    #[arg(long)]
+    match_length: usize,
+}
+
+fn get_record_position(record_number: usize, index_file: &mut File) -> (u64, u64) {
+    let record_offset = record_number as u64 * std::mem::size_of::<u64>() as u64;
+    index_file.seek(SeekFrom::Start(record_offset));
+    let record_offset_start = index_file.read_u64::<NativeEndian>().unwrap();
+    let record_offset_end = index_file.read_u64::<NativeEndian>().unwrap();
+    (record_offset_start, record_offset_end)
+}
+
+fn get_record(
+    record_number: usize,
+    record_file: &mut File,
+    index_file: &mut File,
+) -> serde_json::Value {
+    let (start, end) = get_record_position(record_number, index_file);
+    record_file.seek(SeekFrom::Start(start));
+    let mut buf: Vec<u8> = Vec::with_capacity((end - start) as usize);
+    record_file.read_exact(&mut buf);
+    let json_string = std::str::from_utf8(&buf).expect("Not a valid json string");
+    serde_json::from_str(&json_string).unwrap()
+}
+
+fn main() -> io::Result<()> {
+    let args = Command::parse();
+    let mut record_file = File::open(args.record_file).expect("Can't open ecord file");
+    let mut index_file = File::open(args.record_index_file).expect("Can't open index file");
+    let search_file = File::open(args.search_file).expect("Can't open search file");
+    let file_size = search_file.metadata()?.size() as usize;
+    let records_size = args.match_length * std::mem::size_of::<(u32, f32)>();
+    assert_eq!(file_size % records_size, 0, "Unexpected record length");
+    let num_records = file_size / records_size;
+
+    let mut output_table =
+        Writer::from_path(args.output_table).expect("could not open output table");
+    output_table.write_record(&[
+        "root_file_id",
+        "root_record_id",
+        "file_id",
+        "record_id",
+        "distance",
+    ]);
+
+    for i in 0..num_records {
+        let central_record = get_record(i, &mut record_file, &mut index_file);
+        let root_file_id = central_record.get("FILE_ID").unwrap().as_str().unwrap();
+        let root_record_id = central_record.get("RECORD_ID").unwrap().as_str().unwrap();
+        for _j in 0..args.match_length {
+            let neighbor_id = index_file.read_u32::<NativeEndian>().unwrap();
+            let neighbor_distance = index_file.read_f32::<NativeEndian>().unwrap();
+            let neighbor_record =
+                get_record(neighbor_id as usize, &mut record_file, &mut index_file);
+            let file_id = neighbor_record.get("FILE_ID").unwrap().as_str().unwrap();
+            let record_id = neighbor_record.get("RECORD_ID").unwrap().as_str().unwrap();
+            let distance = format!("{neighbor_distance}");
+
+            output_table
+                .write_record([root_file_id, root_record_id, file_id, record_id, &distance])
+                .expect("Could not write csv record");
+        }
+    }
+
+    Ok(())
+}

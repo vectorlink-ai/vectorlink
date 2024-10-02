69b1a9bc3ffeaaaf9969ce7db68eef049d2bfc7a
Gavin Mendel-Gleason <gavin@terminusdb.com>
2024-10-02 09:42:25 +0200

    Fix build params to be hard coded.

--- hnsw-redux/benches/unquantized_1024.rs
+++ hnsw-redux/benches/unquantized_1024.rs
@@ -14,7 +14,17 @@ fn bench_unquantized_1024_construction(b: &mut Bencher) {
     let number_of_vecs = 10000;
     let vecs = random_vectors(number_of_vecs, 1024, 0x533D);
     let comparator = CosineDistance1024::new(&vecs);
-    let bp = BuildParams::default();
+    let bp = BuildParams {
+        order: 24,
+        neighborhood_size: 24,
+        bottom_neighborhood_size: 48,
+        optimize_sp: SearchParams {
+            parallel_visit_count: 1,
+            visit_queue_len: 100,
+            search_queue_len: 30,
+            circulant_parameter_count: 0,
+        },
+    };
 
     b.iter(|| {
         let _ = Hnsw::generate(&bp, &comparator);
@@ -26,10 +36,25 @@ fn bench_unquantized_1024_search(b: &mut Bencher) {
     let number_of_vecs = 10000;
     let vecs = random_vectors(number_of_vecs, 1024, 0x533D);
     let comparator = CosineDistance1024::new(&vecs);
-    let bp = BuildParams::default();
+    let bp = BuildParams {
+        order: 24,
+        neighborhood_size: 24,
+        bottom_neighborhood_size: 48,
+        optimize_sp: SearchParams {
+            parallel_visit_count: 1,
+            visit_queue_len: 100,
+            search_queue_len: 30,
+            circulant_parameter_count: 0,
+        },
+    };
 
     let hnsw = Hnsw::generate(&bp, &comparator);
-    let sp = SearchParams::default();
+    let sp = SearchParams {
+        parallel_visit_count: 12,
+        visit_queue_len: 100,
+        search_queue_len: 30,
+        circulant_parameter_count: 8,
+    };
     let vec = &random_vectors(1, 1024, 0x12345)[0];
     b.iter(|| hnsw.search_from_initial(Vector::Slice(vec), &sp, &comparator));
 }

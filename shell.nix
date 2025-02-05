31ebe5c1d75ce504594c007766a056bafe77ea62
Maren van Otterdijk <maren@vectorlink.ai>
2025-02-05 16:05:58 +0100

    move venv setup to uv

--- shell.nix
+++ shell.nix
@@ -1,23 +1,11 @@
 {pkgs, mkShell}:
 let
   pythonVenvShellHook = ''
-    # Setup a fresh virtualenv when `direnv reload` is executed.  This is done
-    # to keep the project working after a `nix-collect-garbage` removes the
-    # pointee entry in the nix store. When this happens, the pointer
-    # (e.g. `$VENV/bin/python`) becomes stale and breaks.
-
-    VENV=.venv
-    # Test for the existence of:
-    #   * The existence of the $VENV directory
-    #   * The *POINTEE* of the `$VENV/bin/python` symlink:
-    if [[ ! (-d $VENV  &&  -f $VENV/bin/python) ]]; then
-      rm -rf $VENV  # Remove any existing virtualenv
-      virtualenv $VENV  # Setup a fresh virtualenv
-      source ./$VENV/bin/activate
-      pip install datafusion # install datafusion cause it's useful during development
-      export PYTHONPATH=`pwd`/$VENV/${pkgs.python3.sitePackages}/:$PYTHONPATH
-    fi
-  '';
+cd hnsw_redux-python
+uv sync --no-install-project
+source .venv/bin/activate
+cd ..
+'';
 in
 mkShell {
   buildInputs = with pkgs; [
@@ -26,16 +14,7 @@ mkShell {
     maturin
     openssl
     python3 # For pyo3/maturin, which is used by hnsw_redux-python
-    (python3.withPackages (pythonPkgs: with pythonPkgs; [
-      # Note that even if Python packages like PyTorch or Tensorflow are added,
-      # they will be reinstalled when running `pip -r requirements.txt` because
-      # virtualenv is used below in the shellHook. Fkn virtualenv :/
-      ipython
-      pip
-      setuptools
-      virtualenvwrapper
-      wheel
-    ]))
+    uv
     (rust-bin.nightly."2025-01-23".default.override {
       extensions = [ "rustfmt" "rust-src" "rust-analyzer" ];
       targets = [

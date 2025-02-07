bb9cfd7552f57c47ff75eee2705260c5026211fe
Maren van Otterdijk <maren@vectorlink.ai>
2025-02-07 12:30:11 +0100

    begin work on python code

---
+++ hnsw_redux-python/hnsw_redux/__init__.py
@@ -0,0 +1,11 @@
+import datafusion as df
+import pyarrow as pa
+
+from .hnsw_redux import *
+
+def generate_hnsw_from_vecs(frame: df.DataFrame) -> Hnsw:
+    """Generate a new HNSW from a dataframe that has t least the column 'vector'.
+    Currently only 1536-sized vectors are supported"""
+    vector_frame = frame.select(df.col("vector"))
+
+    # more here..

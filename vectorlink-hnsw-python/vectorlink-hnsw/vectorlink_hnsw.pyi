5ef6e3fb7cfc33f1600bb88cee07da53fb907f4b
Joey Ezechiëls <joey.ezechiels@gmail.com>
2025-02-11 10:16:16 +0100

    Initial work

---
+++ vectorlink-hnsw-python/vectorlink-hnsw/vectorlink_hnsw.pyi
@@ -0,0 +1,42 @@
+#
+
+class Vectors:
+    def __init__(self, data: SimdAlignedAllocationU8, vector_byte_size: int) -> None: ...
+
+    def schafz(self, foo: int) -> None: ...
+
+class SimdAlignedAllocationU8:
+    pass
+
+class LayerMetadata:
+    pass
+
+class Layer:
+    pass
+
+class HnswMetadata:
+    pass
+
+class Hnsw:
+    pass
+
+class HnswType:
+    pass
+
+class Hnsw1024:
+    pass
+
+class Hnsw1536:
+    pass
+
+class IndexConfiguration:
+    pass
+
+class BuildParams:
+    pass
+
+class OptimizationParams:
+    pass
+
+class SearchParams:
+    pass
